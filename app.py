3a072d81cc48ca66ece79924b5fdc4255fcae43b
makaylahgarcia <user.name>
2025-05-02 16:21:47 -0700

    changes -MG

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # configure the database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    "postgresql+psycopg2://testuser:123@localhost:8888/412_group_project"
+    "postgresql+psycopg2://testuser:123@localhost:5432/412_group_project"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -63,6 +63,7 @@ def get_employees():
 @app.route("/products", methods=["GET"])
 def get_products():
     products = Product.query.all()
+
     return jsonify(
         [
             {
@@ -84,26 +85,27 @@ def get_products():
 # get all sales
 @app.route("/sales", methods=["GET"])
 def get_sales():
-    sales = Sale.query.all()
-    # return render_template("sales.html", sales=sales)
+    # sales = Sale.query.all()
+    sales = Sale.query.limit(50).all()
+    return render_template("sales.html", sales=sales)
 
-    return jsonify(
-        [
-            {
-                "salesid": sale.salesid,
-                "salespersonid": sale.salespersonid,
-                "customerid": sale.customerid,
-                "productid": sale.productid,
-                "quantity": sale.quantity,
-                "discount": sale.discount,
-                "totalprice": sale.totalprice,
-                "salesdate": sale.salesdate,
-                "transactionnum": sale.transactionnum,
-            }
-            for sale in sales
-        ]
-    )
+    # return jsonify(
+    #     [
+    #         {
+    #             "salesid": sale.salesid,
+    #             "salespersonid": sale.salespersonid,
+    #             "customerid": sale.customerid,
+    #             "productid": sale.productid,
+    #             "quantity": sale.quantity,
+    #             "discount": sale.discount,
+    #             "totalprice": sale.totalprice,
+    #             "salesdate": sale.salesdate,
+    #             "transactionnum": sale.transactionnum,
+    #         }
+    #         for sale in sales
+    #     ]
+    # )
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, port="4000")
